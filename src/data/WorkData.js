070ca91812b2f35603f007e0885c908af59cd0a8
Gautam Chauhan <gautamchauhan.officialgc@gmail.com>
2025-03-25 18:44:37 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -50,16 +50,6 @@ export const Work = [
     },
     {
         id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    {
-        id:7,
         name:"PokeSite",
         description:"My first Site",
         tags:["html","css","JavaScript"],
